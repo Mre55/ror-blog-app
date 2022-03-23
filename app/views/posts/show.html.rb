746f43ebff92f9aaac5bf6805695ea855fe3ead1
mre55 <misganawmihreteab>
2022-03-23 14:40:41 +0300

    Add posts show.html.rb file

---
+++ app/views/posts/show.html.rb
@@ -0,0 +1,10 @@
+<div> 
+  <h3>Post 1 by username</h3>
+  <p>Comments: x Likes: y</p>
+  <p>post content</p>
+</div>
+<div> 
+  <p>Username: Comment 1</p>
+  <p>Username: Comment 2</p>
+  <p>Username: Comment 3</p>
+</div>
