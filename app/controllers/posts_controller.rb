4fb064e2eda8183036b0ac6ccacbe2379ae6a2c5
mre55 <misganawmihreteab>
2022-04-01 12:44:49 +0300

    Initialize comment counter and like counter to zero when new post is created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts_with_comments = @user.posts.all.map { |post| { post:, comments: post.five_recent_comments } }
+    @posts_with_comments = @user.posts.all.map { |post| { post: post, comments: post.five_recent_comments } }
   end
 
   def show
@@ -17,6 +17,8 @@ class PostsController < ApplicationController
   def create
     @user = current_user
     @post = @user.posts.new(post_params)
+    @post.commentsCounter = 0
+    @post.likesCounter = 0
 
     if @post.save
       flash[:notice] = 'Post was successfully created'
